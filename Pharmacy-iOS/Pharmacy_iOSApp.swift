17cf039ed08a94be44b923f42bfd3f20a4d27988
Mukesh Sharma <mukeshsharma@trugreenmail.com>
2025-03-30 01:18:26 -0400

    Initial Commit

---
+++ Pharmacy-iOS/Pharmacy_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pharmacy_iOSApp.swift
+//  Pharmacy-iOS
+//
+//  Created by Mukesh  Sharma on 2025-03-30.
+//
+
+import SwiftUI
+
+@main
+struct Pharmacy_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
